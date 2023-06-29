9041f71025bee0b93ab6ae7f964cadc8db5cb9a9
3030667 <3030667@30306-67>
2023-06-29 13:54:24 +0900

    TAKASHIMA01

---
+++ ReservationSystem/src/main/java/com/example/reservationsystem/domain/entity/Planinformation.java
@@ -0,0 +1,25 @@
+package com.example.reservationsystem.domain.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="planinfomation")
+
+
+public class Planinformation {
+ @Id
+ private String plancode;
+ private String planname;
+ 
+ private String roomcode;;
+
+
+}
