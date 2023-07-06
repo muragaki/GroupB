9fadcc4d4bb7ec179eb75e02e315fab110d6ce14
3030704 <3030704@30307-04>
2023-07-06 15:39:43 +0900

    raki

--- ReservationSystem/src/main/java/com/example/reservationsystem/app/controller/Admin/Plan/PlanController.java
+++ ReservationSystem/src/main/java/com/example/reservationsystem/app/controller/Admin/Plan/PlanController.java
@@ -1,10 +1,16 @@
 package com.example.reservationsystem.app.controller.Admin.Plan;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.reservationsystem.app.form.Plan.Planform;
+import com.example.reservationsystem.domain.entity.Planinformation;
 import com.example.reservationsystem.domain.service.Plan.PlanService;
 
 @Controller
@@ -12,11 +18,14 @@ import com.example.reservationsystem.domain.service.Plan.PlanService;
 
 public class PlanController {
 	@Autowired
-	PlanService planService ;
+	PlanService planService;
 	
 	@GetMapping("plan")
-	String plan() {
+	String plan(@ModelAttribute Planform planForm ,Model model ) {
+	planForm.setPlanList( (ArrayList<Planinformation>) planService.readAll());	
+	model.addAttribute("planinfomationform",planForm );
 		return "admin/plan/planview";
 	}
+	
 
 }
--- ReservationSystem/src/main/java/com/example/reservationsystem/app/controller/BookingDetails/BookingDetails.java
+++ ReservationSystem/src/main/java/com/example/reservationsystem/app/controller/BookingDetails/BookingDetails.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 import com.example.reservationsystem.domain.entity.Reservation;
 import com.example.reservationsystem.domain.service.BookingDetailsService.BookingDetailsService;
@@ -16,11 +16,11 @@ public class BookingDetails {
 	BookingDetailsService bookingDetailsservice;
 	
 	@GetMapping("/bookingdetails")
-	String example(@RequestParam("reservation")String resnumber, Model model) {
-		//reservation = bookingDetailsservice.example(1);
-		Integer res  = Integer.parseInt(resnumber);
-		Reservation reservation = new Reservation();
-		reservation = bookingDetailsservice.findOne(res);
+	String example(@ModelAttribute Reservation reservation, Model model) {
+//		Integer res  = Integer.parseInt(resnumber);
+		//Reservation reservation = new Reservation();
+		reservation = bookingDetailsservice.example(1);
+//		reservation = bookingDetailsservice.findOne(res);
 		model.addAttribute("reservation",reservation);
 	return "bookingdetails/bookingdetailsview";
 }
