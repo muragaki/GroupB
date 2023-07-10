2856fc8b7cf36f6b20bc5c269d478bc9d695df49
3030689 <3030689@30306-89>
2023-07-10 15:03:46 +0900

    kuwa07

--- ReservationSystem/src/main/java/com/example/reservationsystem/app/controller/twoguest/TwoGuestController.java
+++ ReservationSystem/src/main/java/com/example/reservationsystem/app/controller/twoguest/TwoGuestController.java
@@ -2,8 +2,11 @@ package com.example.reservationsystem.app.controller.twoguest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
+import com.example.reservationsystem.domain.entity.Guest;
 import com.example.reservationsystem.domain.service.TwoGuest.TwoGuestService;
 
 @Controller
@@ -11,18 +14,16 @@ import com.example.reservationsystem.domain.service.TwoGuest.TwoGuestService;
 public class TwoGuestController {
 	@Autowired
 	TwoGuestService twoguestService;
-	
+
 	@GetMapping("twoguest")
 	String reservation2() {
 		return "twoguest/twoguestview";
 	}
+
 	@GetMapping("one")
-	String reservation1() {
+	String reservation1(@ModelAttribute Guest guest, Model model) {
+		model.addAttribute("guest", guest);
 		return "twoguest/oneview";
 	}
-	
-	
-	
-	
 
 }
