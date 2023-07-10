1461160dbc1047417807eadc1c5a5351e1d4fc9f
3030704 <3030704@30307-04>
2023-07-10 15:04:51 +0900

    raki5

--- ReservationSystem/src/main/java/com/example/reservationsystem/app/controller/twoguest/TwoGuestController.java
+++ ReservationSystem/src/main/java/com/example/reservationsystem/app/controller/twoguest/TwoGuestController.java
@@ -2,8 +2,11 @@ package com.example.reservationsystem.app.controller.twoguest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.example.reservationsystem.domain.entity.Guest;
 import com.example.reservationsystem.domain.service.TwoGuest.TwoGuestService;
 
 @Controller
@@ -12,14 +15,17 @@ public class TwoGuestController {
 	@Autowired
 	TwoGuestService twoguestService;
 	
-	@GetMapping("twoguest")
-	String reservation2() {
+	@GetMapping("/twoguest")
+	String example(@RequestParam("guest")String guestcode, Model model) {
+			Guest guest = twoguestService.findOne(guestcode);
+		model.addAttribute("guest",guest);
 		return "twoguest/twoguestview";
 	}
 	@GetMapping("one")
 	String reservation1() {
 		return "twoguest/oneview";
 	}
+	
 	
 	
 	
