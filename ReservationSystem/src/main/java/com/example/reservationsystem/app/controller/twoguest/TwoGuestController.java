e8e6befb08bffa2ce44add72276a924b50939e1f
3030689 <3030689@30306-89>
2023-07-11 13:55:47 +0900

    Merge branch 'master' of https://github.com/muragaki/GroupB

--- ReservationSystem/src/main/java/com/example/reservationsystem/app/controller/twoguest/TwoGuestController.java
+++ ReservationSystem/src/main/java/com/example/reservationsystem/app/controller/twoguest/TwoGuestController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.reservationsystem.app.form.Guest.GuestEditForm;
@@ -35,4 +36,8 @@ public class TwoGuestController {
 		return "twoguest/oneview";
 	}
 
+	@PostMapping("guestsave")
+	String guestsave() {
+		return "guestsave/guestsave";
+	}
 }
